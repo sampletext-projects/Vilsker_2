369a930a6a6ef42084f281451d7aa0a1124647a4
sampletext32 <sampletext32@bk.ru>
2020-04-09 20:42:03 +0300

    Add project

---
+++ Vilsker_2/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N1, N2;
+
+	char c;
+	do
+	{
+		cin >> N1;
+		cin >> N2;
+		for (int i = N1; i <= N2; i++)
+		{
+			//Заканчивается ли число k числом M
+			int m = i;//здесь необходимо скопировать значение, чтобы не потерять его при вычислениях
+			int k = i * i;
+			bool ends = true;
+			while (m != 0)
+			{
+				if (m % 10 != k % 10)
+				{
+					ends = false;
+					break;
+				}
+				m /= 10;
+				k /= 10;
+			}
+
+			if (ends)
+			{
+				cout << i * i << " (" << i << "^2) ends with " << i << endl;
+			}
+		}
+		cout << "Repeat (1)? ";
+		cin >> c;
+	}
+	while (c == '1');
+
+
+	return 0;
+}
